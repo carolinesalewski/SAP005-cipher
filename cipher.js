b170129c5975b71bef296d2d43cfd53fe181e28c
Caroline Shimada <caroline_shimada@hotmail.com>
2020-10-29 20:45:51 -0300

    logicacodificarcorrigida

--- cipher.js
+++ cipher.js
@@ -1,17 +1,17 @@
-function botao(){
+function criptografar(){
+  
+  var chave = "";
   let texto = prompt("Insira o seu primeiro nome");
   let quantidadeDeCasas = prompt ("Insira o primeiro numero da sua idade");
   const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var chave = "";
-
-  for (let i = 0; i < texto.length; i++){
-      
-      let resultado = alfabeto.search(texto.charAt(i).toUpperCase()) + quantidadeDeCasas;
   
+  for (let i = 0; i < texto.length; i++){
+      let resultado = alfabeto.search(texto.charAt(i).toUpperCase()) + parseInt(quantidadeDeCasas);
       if (resultado>25){
           resultado=resultado-24;
-      }    
-      chave = chave + alfabeto.charAt(resultado);
-
-  }
+      }
+    chave = chave + alfabeto.charAt(resultado);
+    }
+  let id = document.getElementById("criptografado");
+  id.textContent = ("Olá "+ texto +" Seu nome em Dothraki é" + chave);
 }
