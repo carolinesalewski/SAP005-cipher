3a3576568d2ac13cd8e5e42d26d350e78cc23d53
Caroline Shimada <caroline_shimada@hotmail.com>
2020-11-03 14:39:32 -0300

    criacaoDom

---
+++ index.js
@@ -0,0 +1,6 @@
+function imprimeNomeCriptografado(){
+    document.getElementById("criptografado").innerHTML = ("Olá "+ texto +" seu nome em Dothraki é <br>" + chave);
+}
+function imprimeNomeDescriptografado(){
+  document.getElementById("descriptografado").innerHTML=("Seu nome não dothraki é <br>" + chaveNome)
+}
