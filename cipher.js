45f12a841f86d6ca48bb22c171662656ae7ad283
Caroline Shimada <caroline_shimada@hotmail.com>
2020-11-06 14:44:03 -0300

    readme

--- cipher.js
+++ cipher.js
@@ -9,11 +9,14 @@ const cipher = {
       throw new TypeError("Check the data type.");
     } // neste IF estou colocando uma condicional para o caso do usuário inserir um tipo de dado inconsistente com o que o programa quer.
 
+    loop:
     for (let i = 0; i < text.length; i++) {
+      if (text.charAt(i) == " ") {
+        textEncoded = textEncoded + " ";
+        continue loop;
+      }
 
       let result = alphabet.search(text.charAt(i).toUpperCase()) + (parseInt(offset) % alphabet.length);
-      //o charAt vai pegar a letra da posição indicada pelo loop no texto inserido, o alphabet.search vai buscar a letra indicada e vai transformar em letra maiuscula. O parseInt vai transformar em numero inteiro a quantidade de offset indicada e ira fazer o módulo da quantidade de letras do alfabeto. ex: offset: 2 text: carol. CharAt pegara posição 0 (c) , transformou em maiuscula (C) e vai buscar pelo alphabet search a posição indicada da palavra(c esta na posição 2 do alfabeto),pegou a quantidade de offset(2) e fez modulo de 26 que da 2. 2+2 = 4
-      //result agora é = 4 ou seja, a posição 4 do alfabeto.
 
       if (result >= alphabet.length) {
         result = result - alphabet.length;
@@ -33,7 +36,12 @@ const cipher = {
       throw new TypeError();
     }
 
+    loop2:
     for (let i = 0; i < textEncoded.length; i++) {
+      if (textEncoded.charAt(i) == " ") {
+        textDecoded = textDecoded + " ";
+        continue loop2;
+      }
 
       let result = alphabet.search(textEncoded.charAt(i).toUpperCase()) - (parseInt(offset) % alphabet.length);
       //estou fazendo o loop usando o resultado do texto codificado 
